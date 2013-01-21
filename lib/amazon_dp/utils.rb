23afbff435f226cdf835de45b207c38503f11a27
kimoto <sub+peerler@gmail.com>
2013-01-21 13:45:38 +0900

    cero対応

--- lib/amazon_dp/utils.rb
+++ lib/amazon_dp/utils.rb
@@ -4,6 +4,7 @@ module AmazonDP
   def self.get(url_or_asin)
     f = Fetcher.new
     f.adult_auth
+    f.cero_auth
     Parser.new.parse_html f.fetch(url_or_asin)
   end
   def self.logger=(logger)
--- lib/amazon_dp/version.rb
+++ lib/amazon_dp/version.rb
@@ -1,3 +1,3 @@
 module AmazonDP
-  VERSION = "0.0.3"
+  VERSION = "0.0.4"
 end
